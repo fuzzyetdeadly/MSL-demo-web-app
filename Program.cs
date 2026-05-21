2c4dce13626a3356f32bd0496cbe03655095bb6f
Timothy Guan <guan.chuan.siong@gmail.com>
2026-05-21 15:07:30 +1000

    feat: add example endpoints with redirect

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello .NET developer community!");
+# region middleware
+// Reroute old page to new
+app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
+# endregion
+
+# region endpoints
+app.MapGet("/", () => "Welcome to Contoso");
+app.MapGet("/about", () => "Contoso was founded in 2020");
+# endregion
 
 app.Run();
