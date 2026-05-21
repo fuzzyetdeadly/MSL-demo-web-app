0ca17323be952b14dcb08131e73018cfe7256ec4
fuzzyetdeadly <guan.chuan.siong@gmail.com>
2026-05-21 15:37:06 +1000

    Feat/configure middleware (#1)
    * feat: add example middlewares

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello .NET developer community!");
+# region middleware
+// Log request/response info
+app.Use(async (context, next) =>
+{
+  await next();
+
+  Console.WriteLine($"{context.Request.Method} {context.Request.Path} {context.Response.StatusCode}");
+});
+
+// Reroute old page to new
+app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
+# endregion
+
+# region endpoints
+app.MapGet("/", () => "Welcome to Contoso");
+app.MapGet("/about", () => "Contoso was founded in 2020");
+# endregion
 
 app.Run();
